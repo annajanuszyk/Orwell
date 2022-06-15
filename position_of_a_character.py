2d753236948014000cab16829b790ec7e26a27a0
annajanuszyk <102989586+annajanuszyk@users.noreply.github.com>
2022-06-15 11:50:02 +0200

    Add files via upload

---
+++ position_of_a_character.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 11:41:54 2022
+
+@author: annajanuszyk
+"""
+
+string = "Hello! This is an example sentence."
+print(string.find("!"))
