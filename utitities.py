90f3d275599fbf3a6d57d25679b688ce3da4b7fe
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2022-06-29 19:34:04 +0200

    Make functions

--- utitities.py
+++ utitities.py
@@ -6,12 +6,40 @@ Created on Wed Jun 15 11:28:28 2022
 @author: annajanuszyk
 """
 
-count = 0;
-str = "Good Morning! This is an example.";
 
-for i in range(0, len(str)):
-    if str[i] in ("!" , "," , "\" , ";" , "/" , "." , "-" , "?"):
-        count = count + 1;
-        
-print("Total number of punctuation marks in string: ");
-print(count);
+def punctuation_marks_in_sentence(sentence):
+    count = 0
+    for s in sentence:
+        if str[i] in r"!,\;/-?.":
+            count = count + 1
+    return count    
+
+
+def words_per_sentence(sentence):
+    return len(string.split())
+
+
+def print_punctuation_marks(sentence):
+    punct_str = ""
+    for s in sentence:
+        if s in string.punctuation:
+            punct_str += s
+
+    return punct_s
+
+
+def character_position(sentence):
+    return sentence.find(char)
+
+
+def word_count(str):
+    counts  = dict()
+    words = str.split()
+    
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+            
+    return counts
