675f8116ce5f56864ba0c66751fd3928c6dda6ef
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2022-06-29 20:00:30 +0200

    Fix variables and imports

--- utilities.py
+++ utilities.py
@@ -5,12 +5,12 @@ Created on Wed Jun 15 11:28:28 2022
 
 @author: annajanuszyk
 """
-
+import unicodedata
 
 def punctuation_marks_in_sentence(sentence):
     count = 0
     for s in sentence:
-        if str[i] in r"!,\;/-?.":
+        if unicode.category(s).startswith("P")
             count = count + 1
     return count    
 
@@ -22,7 +22,7 @@ def words_per_sentence(sentence):
 def print_punctuation_marks(sentence):
     punct_str = ""
     for s in sentence:
-        if s in string.punctuation:
+        if unicode.category(s).startswith("P")
             punct_str += s
 
     return punct_s
@@ -32,7 +32,7 @@ def character_position(sentence):
     return sentence.find(char)
 
 
-def word_count(str):
+def word_count(words):
     counts  = dict()
     words = str.split()
     
